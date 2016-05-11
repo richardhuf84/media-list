9fb5baa1dcba8799b1dc9e60d973f97f3b613ec1
Richard <richardhuf84@gmail.com>
2016-05-11 20:39:35 +1000

    changing things over to use database class

--- includes/config.php
+++ includes/config.php
@@ -57,7 +57,7 @@
 
   $database = new Database();
 
-  $database->query('INSERT INTO mytable (FName, LName, Age, Gender) VALUES (:fname, :lname, :age, :gender)');
+  // $database->query('INSERT INTO mytable (FName, LName, Age, Gender) VALUES (:fname, :lname, :age, :gender)');
 
  /**
   * Global Functions
@@ -128,17 +128,17 @@
      */
 
      // TODO: add back in
-    //  if(isset($_SESSION) && isset($_SESSION['UserID'])) {
-     //
-    //    // READ
-    //    try {
-    //        $results = $db->query("SELECT mediaid, title, year, plot, posterURL, director, genre, media_type FROM media WHERE userID = " . $_SESSION['UserID'] . " ORDER BY mediaid DESC");
-    //    } catch (Exception $e){
-    //        echo "Data could not be retrieved from the database.";
-    //        exit;
-    //    }
-     //
-    //    $mediaList = ($results->fetchAll(PDO::FETCH_ASSOC));
+     if(isset($_SESSION) && isset($_SESSION['UserID'])) {
+
+       // READ
+       try {
+           $medialist = $database->query("SELECT mediaid, title, year, plot, posterURL, director, genre, media_type FROM media WHERE userID = " . $_SESSION['UserID'] . " ORDER BY mediaid DESC");
+       } catch (Exception $e){
+           echo "Data could not be retrieved from the database.";
+           exit;
+       }
+
+      //  $mediaList = ($results->fetchAll(PDO::FETCH_ASSOC));
 
       //  var_dump ()$_SESSION['UserID']);
 
