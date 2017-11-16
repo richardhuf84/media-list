c3dd66476af93ad85206cbaba69564980936ad91
Richard <richardhuf84@gmail.com>
2017-11-17 01:10:26 +1100

    changing from Heroku's JAWSDB back to mysql and PDO

--- includes/config.php
+++ includes/config.php
@@ -27,50 +27,21 @@
 
 
 
-  // JAWSDB
-  //////////////////////////////////////////////////////////////////////////////
+   /**
+    * DB config
+    * Try to connect to the database
+    */
 
-  mysql://ridt9n0etccn6vn8:wo658pu7kfa7zrm7@qbct6vwi8q648mrn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/aq8ja62p0ho96ifa
 
-  // Try to connect to the database
-  // NOTE LOCAL DB connection details.
-  // replacing with live Heroku credentials
-  //  try {
-  //      $db = new PDO("mysql://ridt9n0etccn6vn8:wo658pu7kfa7zrm7@qbct6vwi8q648mrn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/aq8ja62p0ho96ifa","ridt9n0etccn6vn8","wo658pu7kfa7zrm7");
-  //      $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-  //      $db->exec("SET NAMES 'utf8'");
-  //  } catch (Exception $e) {
-  //      echo "Could not connect to database";
-  //      exit;
-  //  }
-
-  $url = getenv('JAWSDB_URL');
-  $dbparts = parse_url($url);
-
-  $hostname = $dbparts['host'];
-  $username = $dbparts['user'];
-  $password = $dbparts['pass'];
-  $database = ltrim($dbparts['path'],'/');
-
-  try {
-    $db = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
-    // set the PDO error mode to exception
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  }
-  catch(PDOException $e)
-  {
-    echo "Connection failed: " . $e->getMessage();
-  }
-
-  // mysqli credentials
-  // $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-  //
-  // $server = $url["host"];
-  // $username = $url["user"];
-  // $password = $url["pass"];
-  // $db = substr($url["path"], 1);
-  //
-  // $conn = new mysqli($server, $username, $password, $db);
+    // Try to connect to the database
+    try {
+        $db = new PDO("mysql:host=localhost;dbname=media;port=8889","root","root");
+        $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+        $db->exec("SET NAMES 'utf8'");
+    } catch (Exception $e) {
+        echo "Could not connect to database";
+        exit;
+    }
 
  /**
   * Global Functions
